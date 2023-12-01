e811ebce53a1e884073c88445a3f066ec3c6f56a
VIJAY KISHOR G P <88893339+VijayKishor6@users.noreply.github.com>
2023-12-01 12:42:04 +0530

    Add project files.

---
+++ MVC_Repository.Core/MVC_DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Repository.Core
+{
+    public class MVC_DbContext:DbContext
+    {
+        public MVC_DbContext(DbContextOptions options ) : base(options)
+        {
+        } 
+        public  DbSet<Shop> Shop { get; set; }      
+    }
+}
---
+++ MVC_Repository.Core/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Repository.Core
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Items { get; set; }
+        [Required]
+        public int Qty { get; set; }
+        [Required]
+        public int Price { get; set; }
+
+
+    }
+}
